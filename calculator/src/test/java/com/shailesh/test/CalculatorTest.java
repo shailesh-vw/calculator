a55361abb30c80961bf0c176534a6524904e6d91
Shailesh <shailesh.waman@gmail.com>
2016-03-08 09:48:31 +0530

    named test method properly

--- calculator/src/test/java/com/shailesh/test/CalculatorTest.java
+++ calculator/src/test/java/com/shailesh/test/CalculatorTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertThat;
 public class CalculatorTest {
 
     @Test
-    public void additionalFunctionalityOfCalculator() {
+    public void additionFunctionalityOfCalculator() {
         Calculator calculator = new Calculator();
         Integer actualSum = calculator.add("1,2");
         assertThat(actualSum, is(3));
